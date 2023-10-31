df4cbe6a91383f4bee1047fe6e92f8b4911dd466
aliaskh4n <149465291+aliaskh4n@users.noreply.github.com>
2023-10-31 14:33:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $request_uri = $_SERVER['REQUEST_URI'];
 
 // Проверяем путь и отправляем соответствующий файл
 if ($request_uri === '/aida') {
-    header('index.html');
+    header('aida.html');
     exit();
 } elseif ($request_uri === '/nurbo') {
     header('political.html');
