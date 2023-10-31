4d18e22c536292e5a15f34de893fed6d6e85d769
aliaskh4n <149465291+aliaskh4n@users.noreply.github.com>
2023-10-31 14:18:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,12 @@ $request_uri = $_SERVER['REQUEST_URI'];
 
 // Проверяем путь и отправляем соответствующий файл
 if ($request_uri === '/aida') {
-    header('aida.html');
+    header('index.html');
     exit();
 } elseif ($request_uri === '/nurbo') {
     header('political.html');
     exit();
 } else {
-    // Если путь не совпадает ни с "/math", ни с "/chem", возвращаем ошибку 404
     header("HTTP/1.0 404 Not Found");
     echo "Страница не найдена";
     exit();
