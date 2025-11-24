5d33a86b16795f2e2c8f2631fd05e15e9f3a99f2
kavunovich <deniwho85@gmail.com>
2025-11-24 16:27:45 +0300

    распаковка монолита

---
+++ game/logics/constants.js
@@ -0,0 +1,4 @@
+export const api_url = 'https://nekros.vercel.app/api';
+export const games_path = '/game/mini-games/games.json';
+export const reload_interval = 30000;
+export const wins_reload_interval = 5000;
