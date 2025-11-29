b47d3bd5f41b8b314302e3b9f6578bb605fd799c
Aliaskhan <149465291+aliaskh4n@users.noreply.github.com>
2025-11-30 03:18:31 +0500

    Update constants.js

--- game/logics/constants.js
+++ game/logics/constants.js
@@ -1,4 +1,4 @@
-export const api_url = 'https://nekros.vercel.app/api';
+export const api_url = 'https://api-taupe-seven.vercel.app/api';
 export const games_path = './mini-games/games.json';
 export const reload_interval = 30000;
 export const wins_reload_interval = 5000;
